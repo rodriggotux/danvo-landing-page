a2242e4600df881d1df9fe128b3fff30413f5cbf
rodriggotux <rodriggotux@outlook.com>
2022-09-05 23:51:03 -0300

    js ok, mas ta dando erro no navegador!

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,21 @@
+/*
 const btn = document.querySelector('.btn-button');
 
 btn.addEventListener('click', () => {
     console.log('passou');
 })
 
+*/
+
+
+function brirmenu() {
+        let  barraMenuAberto = document.querySelector('.btn-button');
+
+        if (barraMenuAberto.style.width == '0px') {
+            barraMenuAberto.style.width = '300px';
+
+        }
+        else {
+            barraMenuAberto.style.width = '0px';
+        }
+}
